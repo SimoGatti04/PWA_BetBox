84fc0356faf5d026c73de3959d3dde56bbc153a4
Simone Gatti <simogatti004@gmail.com>
2024-09-09 11:33:56 +0200

    service-worker

--- service-worker.js
+++ service-worker.js
@@ -9,6 +9,11 @@ const urlsToCache = [
   'images/logo512.png'
 ];
 
+const API_HOSTS = [
+  'https://legally-modest-joey.ngrok-free.app',
+  'http://localhost:3000'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -17,17 +22,34 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(`Fetching: ${event.request.url}`);
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          console.log(`Serving from cache: ${event.request.url}`);
-          return response;
-        }
-        console.log(`Fetching from network: ${event.request.url}`);
-        return fetch(event.request);
-      })
-  );
-});
+  const url = new URL(event.request.url);
 
+  // Controlla se la richiesta è per l'API
+  if (API_HOSTS.includes(url.origin)) {
+    // Per le richieste API, vai direttamente alla rete senza controllare la cache
+    event.respondWith(fetch(event.request));
+  } else {
+    // Per le altre richieste, usa la strategia cache-first
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request).then(
+            (response) => {
+              // Opzionale: memorizza nella cache la nuova risorsa se è una GET
+              if (event.request.method === 'GET') {
+                let responseToCache = response.clone();
+                caches.open(CACHE_NAME)
+                  .then((cache) => {
+                    cache.put(event.request, responseToCache);
+                  });
+              }
+              return response;
+            }
+          );
+        })
+    );
+  }
+});
